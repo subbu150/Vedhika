5425c51393ca6e796eaafdd87bc392fbb8b9d20d
subbu150 <subashbalupunuri@gmail.com>
2026-02-22 14:50:25 +0530

    correcting Reset Password

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,10 +1,22 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
-  // await sendEmail(...);
-console.log("EMAIL SKIPPED");
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
 
-res.json({ message: "Reset email sent successfully" });
+  const mailOptions = {
+    from: `Vedika <${process.env.EMAIL_USER}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  await transporter.sendMail(mailOptions);
 };
 
 export default sendEmail;
