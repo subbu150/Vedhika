56219b43eb90737e960fcd8a3fecbc3468cdf838
subbu150 <subashbalupunuri@gmail.com>
2026-02-22 12:22:00 +0530

    Resolving error while deploying

--- event-frontend/src/features/event/api.js
+++ event-frontend/src/features/event/api.js
@@ -1,5 +1,5 @@
 import api from "../../api/client";
-import Status from "../../features/public/components/stautus.JSX";
+
 /* ---------------- GET ---------------- */
 export const getEventsRequest = async () => {
   const res = await api.get("/events");
