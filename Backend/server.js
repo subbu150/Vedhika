8d181f0f3e72b7c5482f59e73f767b4f893ce68d
subbu150 <subashbalupunuri@gmail.com>
2026-02-22 13:20:11 +0530

    Backend CORS request

--- Backend/server.js
+++ Backend/server.js
@@ -19,12 +19,13 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://vedhika-1.onrender.com"
+  ],
+  credentials: true
+}));
 app.use(morgan("dev"));
 app.use(apiLimiter);
 
