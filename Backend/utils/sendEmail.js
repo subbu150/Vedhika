cf0ec5ce1204090962e00bc573f08bd5545f80ac
subbu150 <subashbalupunuri@gmail.com>
2026-02-22 14:46:04 +0530

    try

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,22 +1,10 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  // await sendEmail(...);
+console.log("EMAIL SKIPPED");
 
-  const mailOptions = {
-    from: `Vedika <${process.env.EMAIL_USER}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-  };
-
-  await transporter.sendMail(mailOptions);
+res.json({ message: "Reset email sent successfully" });
 };
 
 export default sendEmail;
